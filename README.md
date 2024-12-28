bb5b9e34f40a07183f268fb3350180bf454d5398
kion-demiroglu <A0071037@kiongroup.com>
2024-12-28 14:15:47 +0100

    make some refactoring

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 #### to install all the packages of this repo use the command "pip install -r requirements.txt" 
 
 #### python version that is used 3.12.8
+
+We are using a PID dataset which has a pid document with other required information such as its serial number, description, name and what not.
+The coordinate location where the PID document is shown only is at (4946, 2101, 5307, 2880)
--- src/utils/convert_points_to_bounding_box.py
+++ src/utils/convert_points_to_bounding_box.py
@@ -4,18 +4,12 @@ from src.models.bounding_box import BoundingBox
 from src.models.vertex import Vertex
 
 
-def convert_points_to_bounding_box(points: Tuple[float, float, float, float]):
-    arr_list = []
-    for point in points:
-        [x1, y1, x2, y2] = point
-        src = Vertex(x=x1, y=y1)
-        dest = Vertex(x=x2, y=y2)
-        arr_list.append(
-            BoundingBox(
-                name="",
-                pointDest=dest,
-                pointSrc=src
-            )
-        )
-
-    return arr_list
+def convert_points_to_bounding_box(point: Tuple[float, float, float, float]):
+    (x1, y1, x2, y2) = point
+    src = Vertex(x=x1, y=y1)
+    dest = Vertex(x=x2, y=y2)
+    return BoundingBox(
+        name="",
+        pointDest=dest,
+        pointSrc=src
+    )
